740a36bbfbac536c34b62a57e942f018c3405eb2
drak1ng <rodrigo.misquini@gmail.com>
2019-11-29 16:45:56 -0300

    beta

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -76,55 +76,55 @@ $$(document).on('page:init', function (e) {
     }
 
     if(e.detail.el.dataset.page=="quem-somos"){
-        $.get("http://capsulas4u.com.br/app_api/quem-somos.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/quem-somos.php", function(data){
             $("#quem-somos-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="servicos"){
-        $.get("http://capsulas4u.com.br/app_api/servicos.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/servicos.php", function(data){
             $("#servicos-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="como-funciona"){
-        $.get("http://capsulas4u.com.br/app_api/como-funciona.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/como-funciona.php", function(data){
             $("#como-funciona-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="duvidas"){
-        $.get("http://capsulas4u.com.br/app_api/duvidas.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/duvidas.php", function(data){
             $("#duvidas-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="contato"){
-        $.get("http://capsulas4u.com.br/app_api/contato.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/contato.php", function(data){
             $("#contato-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="politica-de-privacidade"){
-        $.get("http://capsulas4u.com.br/app_api/politica-de-privacidade.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/politica-de-privacidade.php", function(data){
             $("#politica-de-privacidade-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="politica-de-cookies"){
-        $.get("http://capsulas4u.com.br/app_api/politica-de-cookies.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/politica-de-cookies.php", function(data){
             $("#politica-de-cookies-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="termos-de-uso"){
-        $.get("http://capsulas4u.com.br/app_api/termos-de-uso.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/termos-de-uso.php", function(data){
             $("#termos-de-uso-content").html(data);
         });
     }
 
     if(e.detail.el.dataset.page=="oque-e-compartilhamento"){
-        $.get("http://capsulas4u.com.br/app_api/oque-e-compartilhamento.php", function(data){
+        $.get("http://novo.querocompartilhar.com.br/app_api/oque-e-compartilhamento.php", function(data){
             $("#oque-e-compartilhamento-content").html(data);
         });
     }
